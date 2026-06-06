c6da3f8fc9ae82e3baae6873c0a50dc637c76491
Huzefamd <45355976+huzefaMD@users.noreply.github.com>
2026-06-06 21:02:30 +0530

    Add initial Python script for seminar

---
+++ seminar/01_print.py
@@ -0,0 +1,7 @@
+# seminar.py
+# My first Python code!
+
+print("Hello! I am learning Python!")
+print("My name is Ravi")
+print("Python is the language of AI")
+print("I am going to build an AI app today")
