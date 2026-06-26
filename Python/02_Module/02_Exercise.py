1a36689f6f06061ba68b7373d98266b68cb56129
Huzefamd <45355976+huzefaMD@users.noreply.github.com>
2026-06-26 22:32:57 +0530

    Create variables and print product information
    Initialized variables for product details and added print statement.

---
+++ Python/02_Module/02_Exercise.py
@@ -0,0 +1,11 @@
+# Create these variables:
+product_name = ???
+price         = ???
+in_stock      = ???
+rating        = ???
+
+# Print this output using f-strings:
+# Product: Wireless Headphones
+# Price: ₹2999
+# In stock: True
+# Rating: 4.6/5.0
